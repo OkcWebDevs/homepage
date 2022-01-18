15d435ade6e2fab5d6ddeb5b9365c2a45bd6819a
Stephen <stephen@vindevcorp.com>
2022-01-18 17:43:15 -0600

    adding headless for puppeteer for heroku build

--- scully.okcwebdevs-site.config.ts
+++ scully.okcwebdevs-site.config.ts
@@ -4,9 +4,9 @@ import { ScullyConfig } from '@scullyio/scully';
 import '@scullyio/scully-plugin-puppeteer';
 
 export const config: ScullyConfig = {
-  projectRoot: "./src",
-  projectName: "okcwebdevs-site",
+  projectRoot: './src',
+  projectName: 'okcwebdevs-site',
   outDir: './dist/static',
-  routes: {
-  }
+  puppeteerLaunchOptions: { headless: true },
+  routes: {},
 };
