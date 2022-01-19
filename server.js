7b877bd9d75f165a08e74dcfa6751e2e3f91eeb5
Stephen <stephen@vindevcorp.com>
2022-01-18 20:15:41 -0600

    removing scully build and running just angular

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/static/'));
+app.use(express.static(__dirname + '/dist/okcwebdevs-site/'));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/static/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/okcwebdevs-site/index.html'));
 });
 app.listen(process.env.PORT || 8081);
