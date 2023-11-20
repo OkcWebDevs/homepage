c2c04664bfaa0f8e0a11d7f5f04bcf256608f492
TheCodingWaffle <stephen@vindevcorp.com>
2023-11-20 11:11:18 -0600

    updating to angular 17 & removing scully

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/okcwebdevs-site/'));
+app.use(express.static(__dirname + '/dist/okcwebdevs-site/browser'));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/okcwebdevs-site/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/okcwebdevs-site/browser/index.html'));
 });
 app.listen(process.env.PORT || 8081);
